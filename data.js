1eac3c81ff6ba7329de3b1f75b2d4c927cac3468
Jason Zajdel <zajdel@adobe.com>
2019-01-28 16:24:32 -0800

    Added support for timezone displays

--- data.js
+++ data.js
@@ -3,4 +3,18 @@ const appData = {
   favicon: 'exampleAssets/favicon.ico',
   logo: 'exampleAssets/testLogo.png',
   title: 'Your Personal Internet Start Page',
+
+  time: {
+    locale: 'en-US',
+    zones: [
+      {
+        region: 'America/Los_Angeles',
+        title: 'Local Time',
+      },
+      {
+        region: 'Europe/Paris',
+        title: 'Paris Time',
+      },
+    ],
+  },
 };
