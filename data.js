1829d0ac9f548d4a680b6db9fa56287ece4d5339
Jason Zajdel <zajdel@adobe.com>
2019-06-10 15:32:17 -0700

    Fully added link groups

--- data.js
+++ data.js
@@ -19,4 +19,79 @@ const appData = {
   },
 
   quoteOfTheDay: true,
+
+  linkGroups: [
+    {
+      title: 'Social Networks',
+      links: [
+        {
+          name: 'Facebook',
+          url: 'https://www.facebook.com/',
+        },
+        {
+          name: 'Linkedin',
+          url: 'https://www.linkedin.com/',
+        },
+        {
+          name: 'Twitter',
+          url: 'https://twitter.com/',
+        },
+      ],
+    },
+    {
+      title: 'Streaming',
+      links: [
+        {
+          name: 'Youtube',
+          url: 'https://www.youtube.com/',
+        },
+        {
+          name: 'Twitch',
+          url: 'https://www.twitch.tv/',
+        },
+        {
+          name: 'Netflix',
+          url: 'https://www.netflix.com/',
+        },
+        {
+          name: 'Hulu',
+          url: 'https://www.hulu.com/',
+        },
+        {
+          name: 'Plex',
+          url: 'https://www.plex.tv/',
+        },
+      ],
+    },
+    {
+      title: 'Other Open Source Projects',
+      links: [
+        {
+          name: 'Remediator Library',
+          url: 'https://github.com/shabubu/remediator',
+        },
+        {
+          name: 'Remediator CLI',
+          url: 'https://github.com/shabubu/remediator-cli',
+        },
+      ],
+    },
+    {
+      title: 'PC Gaming',
+      links: [
+        {
+          name: 'Steam',
+          url: 'https://store.steampowered.com/',
+        },
+        {
+          name: 'Origin',
+          url: 'https://www.origin.com/usa/en-us/store',
+        },
+        {
+          name: 'Epic Games',
+          url: 'https://www.epicgames.com/store/en-US/',
+        },
+      ],
+    },
+  ],
 };
